d09afb59ca08cbbb09267708710e20718d2ad73c
Rohit T P <tprohit9@gmail.com>
2023-11-22 21:28:35 +0530

    music

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import glob
 import random
 from time import sleep
 
+import pygame
 import serial
 
 from audio import Audio
 from constants import LETTER_MAP
 from mistral import Mistral
 
-# ser = serial.Serial('COM3', 9600, timeout=1)
+ser: serial.Serial | None = None
 
 
 def send_to_serial(text):
-    # ser.write(text.encode())
-    # ser.flush()
-    pass
+    if ser is None:
+        return
+
+    ser.write(text.encode())
+    ser.flush()
 
 
 def text_to_coords(answer):
@@ -28,32 +31,65 @@ def text_to_coords(answer):
     return [LETTER_MAP[c] for c in answer]
 
 
+def pre_recording():
+    pygame.mixer.music.pause()
+    send_to_serial("SPIN")
+    # Stop background music
+
+
+def post_recording():
+    pygame.mixer.music.unpause()
+    send_to_serial("STOP")
+    # play background music
+
+
 def main():
+    global ser
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("bg.mp3")
+    pygame.mixer.music.play(-1)
+
+    try:
+        ser = serial.Serial('COM3', 9600, timeout=1)
+    except serial.SerialException:
+        print("Serial port not found. Skipping serial communication.")
+        print("Please connect the Arduino and restart the program.")
+
     audio = Audio(silent_frames=2)
 
-    audio.set_pre_recording(lambda: send_to_serial("SPIN"))
-    audio.set_post_recording(lambda: send_to_serial("STOP"))
+    audio.set_pre_recording(pre_recording)
+    audio.set_post_recording(post_recording)
     audio.set_while_silent(None)
 
-    ghosts = glob.glob("ghosts/*.txt")
-    ghost = random.choice(ghosts)
-
-    mistral = Mistral(ghost)
-
     while True:
+        ghosts = glob.glob("ghosts/*.txt")
+        ghost = random.choice(ghosts)
+
+        mistral = Mistral(ghost)
+
+        while True:
+            try:
+                question = audio.get_transcript()
+                print("Question:", question)
+                answer = mistral(question)
+                print("Answer:", answer)
+                coords = text_to_coords(answer)
+
+                for coord in coords:
+                    send_to_serial(",".join(map(str, coord)))
+                    sleep(10)
+
+            except KeyboardInterrupt:
+                break
+
+        send_to_serial("CENTER")
+
+        print("Press Ctrl+C again to exit. Press any other key to continue.")
         try:
-            question = audio.get_transcript()
-            answer = mistral(question)
-            coords = text_to_coords(answer)
-
-            for coord in coords:
-                send_to_serial(",".join(map(str, coord)))
-                sleep(10)
-
-        except KeyboardInterrupt:
+            input()
+        except KeyboardInterrupt or UnicodeError:
             break
-
-    send_to_serial("CENTER")
 
 
 if __name__ == '__main__':
